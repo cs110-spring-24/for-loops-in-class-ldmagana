9a6b1de2e0c4b054b6edb2d158426efb23bb4d99
ldmagana <ldanielamagana@gmail.com>
2024-02-12 16:16:12 -0800

    for loops in class from VSC

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+for count in range(1,31):
+    if count % 15 == 0:
+        print("FizzBuzz")
+    elif count % 3 == 0:
+        print("Fizz")
+    elif count % 5 == 0:
+        print("Buzz")
+    else: print(count)
+# print out all the numbers between 1 and 30 
+# if the number is a multiple of 3, instead of printing out the number, print out "Fizz"
+# if the number is a multiple of 5, instead of printing out the number, print out "Buzz"
+# if the number is a multiple of 15, instead of printing out the number, print out "FizzBuzz"
+
+
+
---
+++ multiples.py
@@ -0,0 +1,8 @@
+# print out multiples of 3 up to 100
+# using the same loop, try printing out the multiples of 5
+
+for count in range(100):
+    if count % 3 == 0:
+        print(count)
+    elif count % 5 == 0:
+        print(count)
